8b15567430f585d3882b51453609814a370f2797
crainone4037 <rainonecarmine0@gmail.com>
2022-06-12 16:03:02 -0400

    git issue icons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,79 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
 
-};
-getUserRepos();
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        displayRepos(data,user);
+        console.log(data);
+      });
+    });
+  };
+  var userFormEl = document.querySelector("#user-form");
+  var nameInputEl = document.querySelector("#username"); 
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+  var formSubmitHandler = function(event){
+
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+    if(username) {
+      getUserRepos(username);
+      nameInputEl.value ="";
+
+    }else{
+      alert("please enter a GitHub username");
+
+    }
+    console.log(event);
+
+      
+  };
+
+  var displayRepos = function(repos,searchTerm){
+
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for(var i = 0; i < repos.length; i++){
+      var repoName =repos[i].owner.login + "/" + repos[i].name;
+
+      var repoEl = document.createElement("div");
+      repoEl.classList = "list-item flex-row justify-space-between align-center"; 
+
+      var titleEl = document.createElement("span");
+      titleEl.textContent = repoName; 
+
+
+
+      repoEl.appendChild(titleEl);
+
+
+      var statusEl = document.createElement("span")
+      statusEl.classList = "flex-row align-center";
+      if(repos[i].open_issues_count > 0){ 
+        statusEl.innerHTML = "<i class = ' fas fa-times status-icon icon-danger'> </i>" + repos[i].open_issues_count + " issue(s)";
+
+      }else { 
+        statusEl.innerHTML = "<i class = 'fas fa-check-square status-icon icon-sucess'></i>" ;
+      }
+
+    }
+      repoContainerEl.appendChild(repoEl);
+      repoEl.appendChild(statusEl);
+
+      
+
+    }
+    
+
+   
+
+
+  
+
+  userFormEl.addEventListener("submit",formSubmitHandler);
